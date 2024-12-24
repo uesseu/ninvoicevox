fba6b66cbe0f2f38dca1c19ba1d78ce687965125
Nakanishi Shoichiro <sheepwing@kyudai.jp>
2024-12-25 07:31:19 +0900

    Appended __init__.py dictionary class.

--- ninvoicevox/__init__.py
+++ ninvoicevox/__init__.py
@@ -1,3 +1,3 @@
-from .voice import Speaker, SpeakerInfo, get_speaker_info, NameStyle
+from .voice import Speaker, SpeakerInfo, get_speaker_info, NameStyle, Dictionary
 from .talker import Talker
 from .asyncqueue import AsyncQueue
